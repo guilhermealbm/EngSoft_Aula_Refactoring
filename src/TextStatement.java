17351e8f3b077003701afa94c5b781109dbb38c7
guilhermealbm <gui.miranda.13@gmail.com>
2020-10-20 10:55:36 -0300

    Commit 17

--- src/TextStatement.java
+++ src/TextStatement.java
@@ -3,15 +3,28 @@ import java.util.Enumeration;
 public class TextStatement extends Statement {
     public String value(Customer aCustomer) {
         Enumeration rentals = aCustomer.getRentals();
-        String result = "Rental Record for " + aCustomer.getName() +
-                "\n";
+        String result = this.header(aCustomer);
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" +
-                    String.valueOf(each.getCharge()) + "\n";
+            result += this.body(each);
         }
         //add footer lines
+        result += this.footer(aCustomer);
+        return result;
+    }
+
+    public String header(Customer aCustomer) {
+        return "Rental Record for " + aCustomer.getName() + "\n";
+    }
+
+    public String body(Rental each) {
+        return "\t" + each.getMovie().getTitle()+ "\t" +
+                String.valueOf(each.getCharge()) + "\n";
+    }
+
+    public String footer(Customer aCustomer) {
+        String result = "";
         result += "Amount owed is " +
                 String.valueOf(aCustomer.getTotalCharge()) + "\n";
         result += "You earned " +
